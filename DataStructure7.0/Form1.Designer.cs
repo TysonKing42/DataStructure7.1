7d87b7a5fb22f92887d1ffd2291e74e460524eff
TysonKing42 <tyson.king42@gmail.com>
2023-09-06 10:12:02 +0800

    Add project files.

---
+++ DataStructure7.0/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace DataStructure7._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.addBtn = new System.Windows.Forms.Button();
+            this.editBtn = new System.Windows.Forms.Button();
+            this.deleteBtn = new System.Windows.Forms.Button();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.searchBtn = new System.Windows.Forms.Button();
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.dataStructureTxt = new System.Windows.Forms.TextBox();
+            this.categoryTxt = new System.Windows.Forms.TextBox();
+            this.structureTxt = new System.Windows.Forms.TextBox();
+            this.definitionTxt = new System.Windows.Forms.TextBox();
+            this.listView = new System.Windows.Forms.ListView();
+            this.name = new System.Windows.Forms.ColumnHeader();
+            this.category = new System.Windows.Forms.ColumnHeader();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.loadBtn = new System.Windows.Forms.Button();
+            this.searchTxt = new System.Windows.Forms.TextBox();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // addBtn
+            // 
+            this.addBtn.Location = new System.Drawing.Point(12, 130);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(75, 23);
+            this.addBtn.TabIndex = 0;
+            this.addBtn.Text = "Add";
+            this.addBtn.UseVisualStyleBackColor = true;
+            this.addBtn.Click += new System.EventHandler(this.addBtn_Click);
+            // 
+            // editBtn
+            // 
+            this.editBtn.Location = new System.Drawing.Point(12, 159);
+            this.editBtn.Name = "editBtn";
+            this.editBtn.Size = new System.Drawing.Size(75, 23);
+            this.editBtn.TabIndex = 1;
+            this.editBtn.Text = "Edit";
+            this.editBtn.UseVisualStyleBackColor = true;
+            this.editBtn.Click += new System.EventHandler(this.editBtn_Click);
+            // 
+            // deleteBtn
+            // 
+            this.deleteBtn.Location = new System.Drawing.Point(12, 188);
+            this.deleteBtn.Name = "deleteBtn";
+            this.deleteBtn.Size = new System.Drawing.Size(75, 23);
+            this.deleteBtn.TabIndex = 2;
+            this.deleteBtn.Text = "Delete";
+            this.deleteBtn.UseVisualStyleBackColor = true;
+            this.deleteBtn.Click += new System.EventHandler(this.deleteBtn_Click);
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Location = new System.Drawing.Point(12, 217);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(75, 23);
+            this.clearBtn.TabIndex = 3;
+            this.clearBtn.Text = "Clear";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+            // 
+            // searchBtn
+            // 
+            this.searchBtn.Location = new System.Drawing.Point(526, 12);
+            this.searchBtn.Name = "searchBtn";
+            this.searchBtn.Size = new System.Drawing.Size(75, 23);
+            this.searchBtn.TabIndex = 4;
+            this.searchBtn.Text = "Search";
+            this.searchBtn.UseVisualStyleBackColor = true;
+            this.searchBtn.Click += new System.EventHandler(this.searchBtn_Click);
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.Location = new System.Drawing.Point(12, 246);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(75, 23);
+            this.saveBtn.TabIndex = 5;
+            this.saveBtn.Text = "Save";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            this.saveBtn.Click += new System.EventHandler(this.saveBtn_Click);
+            // 
+            // dataStructureTxt
+            // 
+            this.dataStructureTxt.Location = new System.Drawing.Point(620, 130);
+            this.dataStructureTxt.Name = "dataStructureTxt";
+            this.dataStructureTxt.Size = new System.Drawing.Size(100, 23);
+            this.dataStructureTxt.TabIndex = 6;
+            this.dataStructureTxt.TextChanged += new System.EventHandler(this.dataStructureTxt_TextChanged);
+            // 
+            // categoryTxt
+            // 
+            this.categoryTxt.Location = new System.Drawing.Point(620, 159);
+            this.categoryTxt.Name = "categoryTxt";
+            this.categoryTxt.Size = new System.Drawing.Size(100, 23);
+            this.categoryTxt.TabIndex = 7;
+            // 
+            // structureTxt
+            // 
+            this.structureTxt.Location = new System.Drawing.Point(620, 188);
+            this.structureTxt.Name = "structureTxt";
+            this.structureTxt.Size = new System.Drawing.Size(100, 23);
+            this.structureTxt.TabIndex = 8;
+            // 
+            // definitionTxt
+            // 
+            this.definitionTxt.Location = new System.Drawing.Point(620, 218);
+            this.definitionTxt.Multiline = true;
+            this.definitionTxt.Name = "definitionTxt";
+            this.definitionTxt.Size = new System.Drawing.Size(100, 169);
+            this.definitionTxt.TabIndex = 9;
+            // 
+            // listView
+            // 
+            this.listView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.name,
+            this.category});
+            this.listView.FullRowSelect = true;
+            this.listView.Location = new System.Drawing.Point(165, 130);
+            this.listView.MultiSelect = false;
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(252, 223);
+            this.listView.TabIndex = 10;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.View = System.Windows.Forms.View.Details;
+            this.listView.SelectedIndexChanged += new System.EventHandler(this.listView_SelectedIndexChanged_1);
+            // 
+            // name
+            // 
+            this.name.Text = "Name";
+            this.name.Width = 100;
+            // 
+            // category
+            // 
+            this.category.Text = "Category";
+            this.category.Width = 100;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 11;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // loadBtn
+            // 
+            this.loadBtn.Location = new System.Drawing.Point(12, 275);
+            this.loadBtn.Name = "loadBtn";
+            this.loadBtn.Size = new System.Drawing.Size(75, 23);
+            this.loadBtn.TabIndex = 12;
+            this.loadBtn.Text = "Load";
+            this.loadBtn.UseVisualStyleBackColor = true;
+            this.loadBtn.Click += new System.EventHandler(this.loadBtn_Click);
+            // 
+            // searchTxt
+            // 
+            this.searchTxt.Location = new System.Drawing.Point(620, 12);
+            this.searchTxt.Name = "searchTxt";
+            this.searchTxt.Size = new System.Drawing.Size(100, 23);
+            this.searchTxt.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.searchTxt);
+            this.Controls.Add(this.loadBtn);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.listView);
+            this.Controls.Add(this.definitionTxt);
+            this.Controls.Add(this.structureTxt);
+            this.Controls.Add(this.categoryTxt);
+            this.Controls.Add(this.dataStructureTxt);
+            this.Controls.Add(this.saveBtn);
+            this.Controls.Add(this.searchBtn);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.deleteBtn);
+            this.Controls.Add(this.editBtn);
+            this.Controls.Add(this.addBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button addBtn;
+        private Button editBtn;
+        private Button deleteBtn;
+        private Button clearBtn;
+        private Button searchBtn;
+        private Button saveBtn;
+        private TextBox dataStructureTxt;
+        private TextBox categoryTxt;
+        private TextBox structureTxt;
+        private TextBox definitionTxt;
+        private ListView listView;
+        private ColumnHeader name;
+        private ColumnHeader category;
+        private StatusStrip statusStrip1;
+        private ToolStripStatusLabel toolStripStatusLabel1;
+        private Button loadBtn;
+        private TextBox searchTxt;
+    }
+}
---
+++ DataStructure7.0/Form1.cs
@@ -0,0 +1,358 @@
+using System.Xml.Linq;
+
+namespace DataStructure7._0
+{
+    public partial class Form1 : Form
+    {
+        static int maxRecords = 12;
+        static int attributes = 4;
+        int recordCount = 0;
+        string[,] dataRecords = new string[maxRecords, attributes];
+        private int selectedIndex = -1; // Declare selectedIndex at the class level
+
+        public Form1()
+        {
+            InitializeComponent();
+            listView.SelectedIndexChanged += listView_SelectedIndexChanged;
+        }
+        private void DisplayList()
+        {
+            listView.Items.Clear();
+
+            for (int i = 0; i < recordCount; i++)
+            {
+                string[] itemData = new string[2]; // Assuming two columns for name and category
+
+                itemData[0] = dataRecords[i, 0]; // Name
+                itemData[1] = dataRecords[i, 1]; // Category
+
+                ListViewItem item = new ListViewItem(itemData);
+                listView.Items.Add(item);
+            }
+        }
+
+        private void listView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView.SelectedItems.Count > 0)
+            {
+                int resultIndex = listView.SelectedItems[0].Index;
+
+                // Display the information in the appropriate textboxes
+                dataStructureTxt.Text = dataRecords[resultIndex, 0]; // Name
+                categoryTxt.Text = dataRecords[resultIndex, 1];      // Category
+                structureTxt.Text = dataRecords[resultIndex, 2];
+                definitionTxt.Text = dataRecords[resultIndex, 3];
+
+                // Highlight the row in the ListView
+                listView.Items[resultIndex].Selected = true;
+                listView.Select();
+            }
+        }
+        private void DisplayRecord(int index)
+        {
+            if (index >= 0 && index < recordCount)
+            {
+                // Display the selected record's information in the textboxes
+                dataStructureTxt.Text = dataRecords[index, 0]; // Name
+                categoryTxt.Text = dataRecords[index, 1];      // Category
+                structureTxt.Text = dataRecords[index, 2];
+                definitionTxt.Text = dataRecords[index, 3];
+
+                // Set the selectedIndex variable for other operations
+                selectedIndex = index;
+            }
+        }
+
+        private void AddRecord()
+        {
+            if (recordCount < maxRecords)
+            {
+                if (!string.IsNullOrWhiteSpace(dataStructureTxt.Text) &&
+                    !string.IsNullOrWhiteSpace(categoryTxt.Text) &&
+                    !string.IsNullOrWhiteSpace(structureTxt.Text) &&
+                    !string.IsNullOrWhiteSpace(definitionTxt.Text))
+                {
+                    // All text boxes are filled, proceed with adding the record
+                    try
+                    {
+                        dataRecords[recordCount, 0] = dataStructureTxt.Text; // Name
+                        dataRecords[recordCount, 1] = categoryTxt.Text;      // Category
+
+                        // Optionally, you can still save the data to your dataRecords array
+                        dataRecords[recordCount, 2] = structureTxt.Text;
+                        dataRecords[recordCount, 3] = definitionTxt.Text;
+
+                        ListViewItem item = new ListViewItem(new string[] { dataStructureTxt.Text, categoryTxt.Text });
+                        listView.Items.Add(item);
+
+                        recordCount++;
+                        toolStripStatusLabel1.Text = "Record added successfully";
+
+                        // Clear text boxes
+                        ClearTextBoxes();
+                    }
+                    catch
+                    {
+                        toolStripStatusLabel1.Text = "Error adding record";
+                    }
+                }
+                else
+                {
+                    // Not all text boxes are filled, show an error message in the status strip
+                    toolStripStatusLabel1.Text = "Please fill all text boxes";
+                }
+            }
+            else
+            {
+                toolStripStatusLabel1.Text = "The array is FULL";
+            }
+        }
+
+        private void EditRecord(int index)
+        {
+            dataRecords[index, 0] = dataStructureTxt.Text;
+            dataRecords[index, 1] = categoryTxt.Text;
+            dataRecords[index, 2] = structureTxt.Text;
+            dataRecords[index, 3] = definitionTxt.Text;
+        }
+        private void DeleteRecord(int index)
+        {
+            if (MessageBox.Show("Are you sure you want to delete this record?", "Confirm Deletion", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                for (int i = index; i < recordCount - 1; i++)
+                {
+                    for (int j = 0; j < attributes; j++)
+                    {
+                        dataRecords[i, j] = dataRecords[i + 1, j];
+                    }
+                }
+                recordCount--;
+                ClearTextBoxes();
+                DisplayList();
+            }
+        }
+
+
+        private void ClearTextBoxes()
+        {
+            dataStructureTxt.Clear(); ;
+            categoryTxt.Clear();
+            structureTxt.Clear();
+            definitionTxt.Clear(); ;
+
+        }
+
+         private void addBtn_Click(object sender, EventArgs e)
+        {
+            AddRecord();
+            ClearTextBoxes();
+            SortRecords();
+            DisplayList();
+        }
+
+
+        private void editBtn_Click(object sender, EventArgs e)
+        {
+            if (listView.SelectedIndices.Count > 0)
+            {
+                int selectedIndex = listView.SelectedIndices[0];
+                EditRecord(selectedIndex);
+                ClearTextBoxes();
+                SortRecords();
+                DisplayList();
+                toolStripStatusLabel1.Text = "Record edited successfully";
+
+            }
+        }
+
+
+        private void deleteBtn_Click(object sender, EventArgs e)
+        {
+            if (listView.SelectedIndices.Count > 0)
+            {
+                int selectedIndex = listView.SelectedIndices[0];
+                DeleteRecord(selectedIndex);
+            }
+        }
+
+        private void saveBtn_Click(object sender, EventArgs e)
+        {
+            SaveToFile();
+        }
+        private void SaveToFile()
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Binary Files (*.dat)|*.dat|All Files (*.*)|*.*";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    using (BinaryWriter bw = new BinaryWriter(new FileStream(saveFileDialog.FileName, FileMode.Create)))
+                    {
+                        for (int i = 0; i < recordCount; i++)
+                        {
+                            for (int j = 0; j < attributes; j++)
+                            {
+                                bw.Write(dataRecords[i, j]);
+                            }
+                        }
+                    }
+
+                    // Show a success message
+                    toolStripStatusLabel1.Text = "Data saved successfully to file.";
+                }
+                catch (Exception ex)
+                {
+                    toolStripStatusLabel1.Text = "Error saving data to file: " + ex.Message;
+                }
+            }
+        }
+
+        private void statusStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+        
+        private void SortRecords()
+        {
+            for (int i = 0; i < recordCount - 1; i++)
+            {
+                for (int j = 0; j < recordCount - i - 1; j++)
+                {
+                    if (string.Compare(dataRecords[j, 0], dataRecords[j + 1, 0]) > 0)
+                    {
+                        SwapRecords(j);
+                    }
+                }
+            }
+
+        }
+        private void SwapRecords(int index)
+        {
+            for (int i = 0; i < attributes; i++)
+            {
+                string temp = dataRecords[index, i];
+                dataRecords[index, i] = dataRecords[index + 1, i];
+                dataRecords[index + 1, i] = temp;
+            }
+        }
+
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            ClearTextBoxes();
+        }
+        
+        private int BinarySearch(string targetName)
+        {
+            int low = 0;
+            int high = recordCount - 1;
+
+            while (low <= high)
+            {
+                int mid = (low + high) / 2;
+                int compareResult = string.Compare(dataRecords[mid, 0], targetName);
+
+                if (compareResult == 0)
+                {
+                    return mid; // Found the record
+                }
+                else if (compareResult < 0)
+                {
+                    low = mid + 1; // Search in the right half
+                }
+                else
+                {
+                    high = mid - 1; // Search in the left half
+                }
+            }
+
+            return -1; // Not found
+        }
+        private void LoadFromFile()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Binary Files (*.dat)|*.dat|All Files (*.*)|*.*";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    using (BinaryReader br = new BinaryReader(new FileStream(openFileDialog.FileName, FileMode.Open)))
+                    {
+                        recordCount = 0;
+                        while (br.BaseStream.Position != br.BaseStream.Length)
+                        {
+                            for (int j = 0; j < attributes; j++)
+                            {
+                                dataRecords[recordCount, j] = br.ReadString();
+                            }
+                            recordCount++;
+                        }
+                    }
+
+                    // Refresh the ListView and show a success message
+                    DisplayList();
+                    toolStripStatusLabel1.Text = "Data loaded successfully from file.";
+                }
+                catch (Exception ex)
+                {
+                    toolStripStatusLabel1.Text = "Error loading data from file: " + ex.Message;
+                }
+            }
+        }
+
+        private void dataStructureTxt_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void statusStrip2_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void loadBtn_Click(object sender, EventArgs e)
+        {
+            LoadFromFile();
+        }
+
+        private void listView_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void searchBtn_Click(object sender, EventArgs e)
+        {
+            string targetName = searchTxt.Text; // Get the target name from the textbox
+
+            if (string.IsNullOrWhiteSpace(targetName))
+            {
+                toolStripStatusLabel1.Text = "Please enter a search term.";
+                return;
+            }
+
+            int resultIndex = BinarySearch(targetName);
+
+            if (resultIndex != -1)
+            {
+                // Display the information in the appropriate textboxes
+                dataStructureTxt.Text = dataRecords[resultIndex, 0]; // Name
+                categoryTxt.Text = dataRecords[resultIndex, 1];      // Category
+                structureTxt.Text = dataRecords[resultIndex, 2];
+                definitionTxt.Text = dataRecords[resultIndex, 3];
+
+                // Highlight the row in the ListView
+                listView.Items[resultIndex].Selected = true;
+                listView.Select();
+
+                toolStripStatusLabel1.Text = "Record found.";
+            }
+            else
+            {
+                ClearTextBoxes();
+                toolStripStatusLabel1.Text = "Record not found.";
+            }
+        }
+    }
+}
